eb444fd19ae190b39d133216283d3341ca5e3f24
Andrew Semenov <semenov.an.sh@gmail.com>
2023-11-12 00:22:04 +0300

    first commit

---
+++ SavannaABC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SavannaABC
+//
+//  Created by Андрей Семенов on 12.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
