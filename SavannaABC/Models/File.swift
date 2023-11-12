792889d3a1c95efa8ff450239d04c09c0f33655d
Andrew Semenov <semenov.an.sh@gmail.com>
2023-11-12 22:39:32 +0300

    add new views

---
+++ SavannaABC/Models/File.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  SavannaABC
+//
+//  Created by Андрей Семенов on 12.11.2023.
+//
+
+import Foundation
+
+struct K {
+    struct Identifiers {
+        static let goToNewsItem: String = "goToNewsItem"
+    }
+}
